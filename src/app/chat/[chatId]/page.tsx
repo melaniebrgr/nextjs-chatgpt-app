de7395d9cceccb7400585e9bff9118deb195c4dc
Melanie Burger <melanie.brgr@gmail.com>
2024-12-29 21:54:01 +0100

    chore: :label: updates params type

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -4,31 +4,31 @@ import { getUserIdFromCookie } from "@/lib/auth/utils";
 import { notFound, redirect } from "next/navigation";
 
 interface ChatDetailsRouteProps {
-    params: {
-        chatId: number;
-    };
+  params: Promise<{
+    chatId: number;
+  }>;
 }
 
 export default async function ChatDetailsRoute({ params }: ChatDetailsRouteProps) {
-    const { chatId } = await params
-    
-    let chat, messages
-    try {
-      const response = await getChatByIdAndMessages(chatId)
-      chat = response.chat
-      messages = response.messages
-    } catch {
-      notFound()
-    }
-
-    const userId = await getUserIdFromCookie()
-    if (userId !== chat.user_id) {
-      redirect('/')
-    }
-
-    return (
-      <main className="p-5">
-        <Chat id={chat.id} messages={messages} />
-      </main>
-    );
+  const { chatId } = await params
+  
+  let chat, messages
+  try {
+    const response = await getChatByIdAndMessages(chatId)
+    chat = response.chat
+    messages = response.messages
+  } catch {
+    notFound()
   }
+
+  const userId = await getUserIdFromCookie()
+  if (userId !== chat.user_id) {
+    redirect('/')
+  }
+
+  return (
+    <main className="p-5">
+      <Chat id={chat.id} messages={messages} />
+    </main>
+  );
+}
